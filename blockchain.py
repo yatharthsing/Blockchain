37e46d5d08fe4d6ac2e6f922d04b998a1ed769ad
Ozymandias <147438439+yatharthsing@users.noreply.github.com>
2026-01-06 22:44:42 +0530

    Add files via upload

---
+++ blockchain.py
@@ -0,0 +1,52 @@
+import hashlib
+import time
+
+class Block:
+    def __init__(self, index, timestamp, data, previous_hash):
+        self.index = index
+        self.timestamp = timestamp
+        self.data = data
+        self.previous_hash = previous_hash
+        self.hash = self.calculate_hash()
+
+    def calculate_hash(self):
+        block_string = (
+            str(self.index) +
+            str(self.timestamp) +
+            str(self.data) +
+            str(self.previous_hash)
+        )
+        return hashlib.sha256(block_string.encode()).hexdigest()
+
+class Blockchain:
+    def __init__(self):
+        self.chain = [self.create_genesis_block()]
+
+    def create_genesis_block(self):
+        return Block(0, time.time(), "Genesis Block", "0")
+
+    def get_latest_block(self):
+        return self.chain[-1]
+
+    def add_block(self, data):
+        prev_block = self.get_latest_block()
+        self.chain.append(
+            Block(
+                prev_block.index + 1,
+                time.time(),
+                data,
+                prev_block.hash
+            )
+        )
+
+    def is_valid(self):
+        for i in range(1, len(self.chain)):
+            current = self.chain[i]
+            previous = self.chain[i - 1]
+
+            if current.hash != current.calculate_hash():
+                return False
+            if current.previous_hash != previous.hash:
+                return False
+
+        return True
